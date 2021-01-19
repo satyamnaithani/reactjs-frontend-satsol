306712df3cd8a3960778007c9b09ea0892bc1343
Satyam Naithani <satyamnaithani99@gmail.com>
2021-01-19 13:10:11 +0530

    Update Deposits.js

--- src/component/dashboard/Deposits.js
+++ src/component/dashboard/Deposits.js
@@ -18,35 +18,35 @@ export default function Deposits() {
   const [data, setData] = useState('');
   const [loading, setLoading] = useState(true)
   const classes = useStyles();
-//   useEffect(() => {
-//     axios({
-//       method: 'GET',
+  useEffect(() => {
+    axios({
+      method: 'GET',
   
-//       url: url + '/sales/recent',
-//       headers: {'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token')).token}
-//   })
-//       .then(response => {
-//           setData(response.data[0])
-//           setLoading(false)
-//       })
-//       .catch(error => {console.log(error)
-//       alert(error)})
-//   }, []);
+      url: url + '/sales/recent',
+      headers: {'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token')).token}
+  })
+      .then(response => {
+          setData(response.data[0])
+          setLoading(false)
+      })
+      .catch(error => {console.log(error)
+      alert(error)})
+  }, []);
  // console.log(data)
   return (
     <React.Fragment>
       
       <Title>Recent Sale</Title>
-//       <Typography component="p" variant="h4">
-//         {loading?<Skeleton animation="wave" />: '₹'+data.grandTotal == undefined ? 0 : data.grandTotal}
-//       </Typography>
-//       <Typography color="textSecondary" className={classes.depositContext}>
-//         {loading?<Skeleton animation="wave" />:data.date === undefined ? '' :'on '+data.date.split('T')[0].split('-')[2]+'/'+data.date.split('T')[0].split('-')[1]+'/'+data.date.split('T')[0].split('-')[0]} 
-//         <br/>
-//         {loading?<Skeleton animation="wave" />:'to '+data.customerName == undefined ? '' : data.customerName}
-//         <br/>
-//         {loading?<Skeleton animation="wave" />:'by '+data.addedBy == undefined ? '' : data.addedBy}
-//       </Typography>
+      <Typography component="p" variant="h4">
+        {loading?<Skeleton animation="wave" />: '₹'+ data.grandTotal}
+      </Typography>
+      <Typography color="textSecondary" className={classes.depositContext}>
+        {loading?<Skeleton animation="wave" />:data.date === undefined ? '' :'on '+data.date.split('T')[0].split('-')[2]+'/'+data.date.split('T')[0].split('-')[1]+'/'+data.date.split('T')[0].split('-')[0]} 
+        <br/>
+        {loading?<Skeleton animation="wave" />:'to '+ data.customerName}
+        <br/>
+        {loading?<Skeleton animation="wave" />:'by '+ data.addedBy}
+      </Typography>
     </React.Fragment>
   );
 }
