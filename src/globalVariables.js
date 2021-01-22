652f57a60719476d555ff83d81a4ee48c9f6982e
satyamnaithani <satyamnaithani99@gmail.com>
2021-01-23 00:08:21 +0530

    update

--- src/globalVariables.js
+++ src/globalVariables.js
@@ -1,5 +1,5 @@
 export const listValue = {
   value: "",
 };
-//export const url = "https://nodejs-backend-satsol.herokuapp.com";
-export const url = "http://localhost:3008";
+export const url = "https://nodejs-backend-satsol.herokuapp.com";
+//export const url = "http://localhost:3008";
