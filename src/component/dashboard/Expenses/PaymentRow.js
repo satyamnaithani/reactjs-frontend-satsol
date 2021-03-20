bc9c447ab421d0b4f356ee11bc21cfc3c2ecf670
satyamnaithani <satyamnaithani99@gmail.com>
2021-03-21 02:24:23 +0530

    added dialog in payment section

--- src/component/dashboard/Expenses/PaymentRow.js
+++ src/component/dashboard/Expenses/PaymentRow.js
@@ -1,19 +1,79 @@
 import React from "react";
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import { makeStyles } from '@material-ui/core/styles';
+import PendingButton from '@material-ui/icons/ErrorOutlined';
+import CompleteButton from '@material-ui/icons/DoneAllOutlined';
+import OpenIcon from '@material-ui/icons/Launch';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+export default function PaymentTable({row}) {
+  const classes = useStyles();
+  const completed = true;
+  const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-export default function PaymentTable({data}) {
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <TableRow key={row.name}>
-        <TableCell component="th" scope="row">
-            {row.name}
-        </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
-        <TableCell align="right">{row.lauda}</TableCell>
+    <TableRow key={row._id}>
+      <TableCell align="center"><OpenIcon className={classes.button} onClick={() => {handleClickOpen()}}/></TableCell>
+      <TableCell component="th" scope="row">
+          {row.billNo}
+      </TableCell>
+      <TableCell align="center">{row.vendor}</TableCell>
+      <TableCell align="center">{row.item}</TableCell>
+      <TableCell align="center">{new Date(row.billDate).toLocaleDateString()}</TableCell>
+      <TableCell align="left">{parseFloat(row.purchaseRate * row.quantity).toFixed(2)}</TableCell>
+      <TableCell align="center">{parseFloat(row.purchaseRate * row.quantity).toFixed(2)}</TableCell>
+      <TableCell align="center">{row.billNo == '0' ? <PendingButton style={{ color: completed ? 'red' :'#FFF720' }} /> : <CompleteButton style={{ color: 'green' }} />}</TableCell>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Transaction Details</DialogTitle>
+        <DialogContent className={classes.dailog}>
+          <form>
+            <Grid container spacing={3}>
+              <Grid item xs={12}><TextField label="Payment Amount" fullWidth variant="outlined" /></Grid>
+              <Grid item xs={12}></Grid>
+              <Grid item xs={12}>
+                <TextField label="Payment Date" variant="outlined" fullWidth InputLabelProps={{shrink: true}}type="date"/>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel id="mode">Payment Mode</InputLabel>{' '}
+                  <Select fullWidth variant="outlined" id="mode" label="Payment Mode">
+                    <MenuItem value={'Online'}>Online</MenuItem>
+                    <MenuItem value={'Cheque'}>Cheque</MenuItem>
+                    <MenuItem value={'Cash'}>Cash</MenuItem>
+                    <MenuItem value={'Others'}>Others</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label="Payment Details" fullWidth multiline rows={4} variant="outlined"/>
+              </Grid>
+            </Grid>
+          </form>
+        </DialogContent>
+      </Dialog>
     </TableRow>
   );
 }
+const useStyles = makeStyles(() => ({
+  button: {
+      cursor: 'pointer'
+  },
+  dailog: {
+    overflowY: 'hidden'
+  }
+}));
--- src/component/dashboard/Expenses/PaymentTable.js
+++ src/component/dashboard/Expenses/PaymentTable.js
@@ -8,7 +8,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-
+import PaymentRow from './PaymentRow';
 
 export default function PaymentTable({title}) {
     const classes = useStyles();
@@ -34,29 +34,20 @@ export default function PaymentTable({title}) {
             <Table className={classes.table} size="small" aria-label="a dense table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Lot</TableCell>
+                        <TableCell>View</TableCell>
+                        <TableCell>Bill No</TableCell>
                         <TableCell align="center">{title}</TableCell>
                         <TableCell align="center">Item</TableCell>
                         <TableCell align="center">Date</TableCell>
                         <TableCell align="center">Bill Amount</TableCell>
-                        <TableCell align="center">Update Amount</TableCell>
+                        <TableCell align="center">Amount Remaining</TableCell>
                         <TableCell align="center">Payment Status</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                {purchaseData.map((row) => (
-                    <TableRow className="classes.tableBody" key={row._id}>
-                    <TableCell component="th" scope="row">
-                        {row.lotNo}
-                    </TableCell>
-                    <TableCell align="center">{row.vendor}</TableCell>
-                    <TableCell align="center">{row.item}</TableCell>
-                    <TableCell align="center">{new Date(row.billDate).toLocaleDateString()}</TableCell>
-                    <TableCell align="left">{parseFloat(row.purchaseRate * row.quantity).toFixed(2)}</TableCell>
-                    <TableCell align="center"></TableCell>
-                    <TableCell align="center"></TableCell>
-                    </TableRow>
-                ))}
+                    {purchaseData.map((row) => (
+                        <PaymentRow key={row._id} row={row}/>
+                    ))}
                 </TableBody>
             </Table>
         </TableContainer>
@@ -82,8 +73,5 @@ const useStyles = makeStyles(() => ({
     },
     row: {
         backgroundColor: '#edffec'
-    },
-    tableBody: {
-        overFlowY: 'scroll'
     }
   }));
