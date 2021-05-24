d1e184d90d44a32bf2c05fafa240b8d3dd36cee8
satyamnaithani <satyamnaithani99@gmail.com>
2021-05-25 01:47:49 +0530

    resolved indentation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
-import React, {
-  Suspense
-} from 'react';
-import {
-  Switch,
-  Route
-} from "react-router-dom";
+import React, { Suspense } from 'react';
+import { Switch, Route } from "react-router-dom";
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Dashboard from './component/dashboard/Dashboard'
 import Stock from './component/dashboard/Stock/Stock'
@@ -21,54 +16,20 @@ import ExpensesM from './component/dashboard/Expenses/ExpensesM'
 const FormLogin = React.lazy(() => import('./component/Form'))
 
 export default function App() {
-
   const matches = useMediaQuery('(min-width:600px)');
-
-  return ( <
-    div className = 'App' >
-    <Switch >
-    <Route exact path = "/" >
-    <Suspense fallback = {<div / >} >
-    <FormLogin / >
-    </Suspense> 
-    < /Route > <
-    Route path = "/dashboard" > {
-      matches ? < Dashboard / > : < DashboardM / >
-    } <
-    /Route> <
-    Route exact path = "/stock" > {
-      matches ? < Stock / > : < StockM / >
-    } <
-    /Route> <
-    Route exact path = "/sales" > {
-      matches ? < Sales / > : < SalesM / >
-    } <
-    /Route> <
-    Route exact path = "/vendors" >
-    <
-    Vendors / >
-    <
-    /Route> <
-    Route exact path = "/customers" >
-    <
-    Customer / >
-    <
-    /Route> <
-    Route exact path = "/item_master" >
-    <
-    ItemMaster / >
-    <
-    /Route> <
-    Route exact path = "/purchase" > {
-      matches ? < Expenses / > : < ExpensesM / >
-    } <
-    /Route> <
-    Route path = "*" >
-    <
-    div > Invalid route < /div> < /
-    Route > <
-    /Switch> 
-    < /
-    div >
+  return ( 
+    <div className = 'App' >
+		<Switch>
+			<Route exact path = "/" ><Suspense fallback = {<div />}><FormLogin /></Suspense></Route> 
+			<Route path = "/dashboard" > {matches ? < Dashboard /> : < DashboardM />} </Route> 
+			<Route exact path = "/stock" > { matches ? < Stock /> : < StockM />} </Route> 
+			<Route exact path = "/sales" > {matches ? < Sales /> : < SalesM />}</Route> 
+			<Route exact path = "/vendors" ><Vendors /></Route> 
+			<Route exact path = "/customers" ><Customer /></Route> 
+			<Route exact path = "/item_master" ><ItemMaster /></Route> 
+			<Route exact path = "/purchase" > {  matches ? < Expenses /> : < ExpensesM />}</Route> 
+			<Route path = "*" ><div> Invalid route </div> </Route>
+		</Switch> 
+    </div>
   );
 }
