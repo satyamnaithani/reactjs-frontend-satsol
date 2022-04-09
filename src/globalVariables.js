2a45a790fc753abaca6cc90680c97317be20cd9f
Satyam Naithani <satyamnaithani99@gmail.com>
2022-04-09 14:09:51 +0530

    lko instance

--- src/globalVariables.js
+++ src/globalVariables.js
@@ -1,5 +1,6 @@
 export const listValue = {
   value: "",
 };
-export const url = "https://nodejs-backend-satsol.herokuapp.com";
+//export const url = "https://nodejs-backend-satsol.herokuapp.com";
+export const url = "https://ss-lko.herokuapp.com";
 //export const url = "http://localhost:3008";
