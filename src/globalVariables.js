3e78ce976eddcb3778aec70960a1dea04c6baae1
Satyam Naithani <satyamnaithani99@gmail.com>
2022-12-08 23:21:00 +0530

    Update globalVariables.js

--- src/globalVariables.js
+++ src/globalVariables.js
@@ -2,4 +2,5 @@ export const listValue = {
   value: "",
 };
 //export const url = "https://nodejs-backend-satsol.herokuapp.com";
-export const url = "http://localhost:3008";
+//export const url = "http://localhost:3008";
+export const url = "https://nodejs-backend-satsol-production.up.railway.app";
